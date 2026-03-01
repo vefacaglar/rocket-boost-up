b8273409c2a469184346a08a4070a89952a1f1f7
Vefa Caglar Ozcan <vefacaglarozcan@gmail.com>
2026-03-01 23:13:07 +0300

    rotation of rocket

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,26 +4,43 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotate;
     [SerializeField] float thrustStrength = 100f;
+    [SerializeField] float rotationStrength = 100f;
 
     Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePositionZ
+                       | RigidbodyConstraints.FreezeRotationX
+                       | RigidbodyConstraints.FreezeRotationY;
     }
 
     private void OnEnable()
     {
         thrust.Enable();
+        rotate.Enable();
     }
 
     void FixedUpdate()
     {
-        if (thrust.ReadValue<float>() > 0)
-        {
-            rb.AddRelativeForce(Vector3.up * Time.fixedDeltaTime * thrustStrength);
-        }
+        ProcessThrusting();
+        ProcessRotation();
     }
 
+    private void ProcessRotation()
+    {
+        float rotationInput = rotate.ReadValue<float>();
+        rb.AddTorque(Vector3.forward * (-rotationInput * rotationStrength * Time.fixedDeltaTime));
+    }
+
+    private void ProcessThrusting()
+    {
+        if (thrust.ReadValue<float>() > 0)
+        {
+            rb.AddRelativeForce(Vector3.up * (thrustStrength * Time.fixedDeltaTime));
+        }
+    }
 }
